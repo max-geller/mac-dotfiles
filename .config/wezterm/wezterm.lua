059273aed52117d4650d685c21e035149bbc13c3
Max Geller <mxgeller@gmail.com>
2024-08-19 09:26:04 -0700

    update wezterm sizing defaults

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,24 +3,26 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config = {
-  default_cwd = "/Users/maxgeller/code",
+  default_cwd = "/Users/maxgeller/code",  -- Set Default Working Directory
+  initial_rows = 50, -- Set Default Window Height
+  initial_cols = 175, -- Set Default Window Width
   automatically_reload_config = true,
-  enable_tab_bar = false,
+  enable_tab_bar = false,  -- Remove Tab Functionality by Default
   window_close_confirmation = "NeverPrompt",
-  window_decorations = "RESIZE",
+  window_decorations = "RESIZE",  -- Eliminates Standard Mac Window Management
   default_cursor_style = "BlinkingBar",
   color_scheme = "Nord (Gogh)",
-  window_background_opacity = 0.88,
-  macos_window_background_blur = 07,
+  window_background_opacity = 0.88,  -- Set Window Opacity
+  macos_window_background_blur = 07, -- Set Window Background Blur
   font = wezterm.font("JetBrains Mono", {weight = "Bold"}),
   font_size = 12.5,
   window_padding = {
     left = 5,
     right = 5,
-    top = 3,
+    top = 5,
     bottom = 3,
   },
-  default_prog = { 'zsh', '-c', '/opt/homebrew/bin/neofetch; exec zsh' }
+  default_prog = { 'zsh', '-c', '/opt/homebrew/bin/neofetch; exec zsh' }  -- Automatically Run NeoFetch When Opening New Window
 }
 
 return config;
